b7da5780dc9f92d7f22db5ed7046b6f014bbb275
DiegoMero <diegomero_1999@hotmail.com>
2023-12-15 13:35:27 -0500

    Add header style

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,8 @@
 import './style/header.scss';
-import backgroundImage from '/public/pic1.png';
+import backgroundImage from '/public/pic3.jpg';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Header() {
   return (
@@ -15,18 +18,8 @@ export default function Header() {
         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
         Pellentesque vestibulum vitae ipsum sed malesuada. 
         Donec feugiat magna odio, ac finibus libero sagittis eu. 
-        Nulla nec risus iaculis tellus maximus volutpat at sit amet mi. 
-        Integer ac justo et lorem gravida malesuada et vel velit. 
-        Vivamus varius lacus nec risus tincidunt, in fringilla velit aliquam. 
-        Vestibulum fringilla, dolor id hendrerit ultrices, lectus turpis gravida risus, et vestibulum ante massa nec orci. 
-        Vestibulum id blandit magna, et sagittis erat. 
-        Pellentesque a sem ultrices, dapibus enim nec, consectetur justo. 
-        Nunc condimentum nisl et augue euismod, nec ultricies eros volutpat. 
-        Vivamus in eros libero. Nunc pharetra leo et lacus sodales, eget sodales ligula facilisis. 
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. 
-        Nam tristique metus at pretium fermentum. 
-        Sed quis quam non est consectetur hendrerit.
       </p>
+      <button className={inter.className}>Conoce más</button>  
     </section>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <Header />
+      <main>
+        <Header />
+      </main>
     </>
   )
 }
