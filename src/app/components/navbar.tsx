b9bb2449c60a861c118d0a99cf914e76d172b67e
DiegoMero <diegomero_1999@hotmail.com>
2023-12-18 15:28:06 -0500

    Add smooth scroll

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,14 +1,32 @@
+import About from './about';
 import './style/navbar.scss';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <nav>
       <h1>LOGO</h1>
       <ul>
-        <li><h3>Sobre Nosotros</h3></li>
-        <li><h3>Servicios</h3></li>
-        <li><h3>Noticias</h3></li>
-        <li><h3>Contacto</h3></li>
+        <li>
+          <Link href="#about">
+            <h3>Sobre Nosotros</h3>
+          </Link>
+        </li>
+        <li>
+          <Link href="#services">
+            <h3>Servicios</h3>
+          </Link>
+        </li>
+        <li>
+          <Link href="#news">
+            <h3>Noticias</h3>
+          </Link>
+        </li>
+        <li>
+          <Link href="#contact">
+            <h3>Contacto</h3>
+          </Link>
+        </li> 
       </ul>
     </nav>
   )
