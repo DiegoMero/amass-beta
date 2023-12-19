583645fbbdd0fd236caa4c6939523b2caae58409
DiegoMero <diegomero_1999@hotmail.com>
2023-12-19 17:26:33 -0500

    Add nav mobile style

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,15 +1,40 @@
-import About from './about';
+"use client";
+
 import './style/navbar.scss';
+import { SlMenu } from "react-icons/sl";
 import Link from 'next/link';
+import React, { useState } from 'react';
+import { RxCross1 } from "react-icons/rx";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    toggleScrollLock();
+  };
+
+  const toggleScrollLock = () => {
+    // Obtén el elemento del cuerpo
+    const body = document.body;
+    
+    // Verifica si el mensaje está visible
+    if (showMenu) {
+      // Desbloquea el scroll al quitar la clase
+      body.classList.remove('scroll-lock');
+    } else {
+      // Bloquea el scroll al agregar la clase
+      body.classList.add('scroll-lock');
+    }
+  };
+
   return (
     <nav>
       <h1>LOGO</h1>
-      <ul>
+      <ul className='desktop-menu'>
         <li>
           <Link href="#about">
-            <h3>Sobre Nosotros</h3>
+            <h3>Nosotros</h3>
           </Link>
         </li>
         <li>
@@ -26,8 +51,34 @@ export default function Navbar() {
           <Link href="#contact">
             <h3>Contacto</h3>
           </Link>
-        </li> 
+        </li>
       </ul>
+      <SlMenu onClick={toggleMenu} />
+      {showMenu && (
+        <ul className='mobile-menu'>
+          <li className='exit'><RxCross1 onClick={toggleMenu}/></li>
+          <li>
+            <Link href="#about">
+              <h3 onClick={toggleMenu}>Nosotros</h3>
+            </Link>
+          </li>
+          <li>
+            <Link href="#services">
+              <h3 onClick={toggleMenu}>Servicios</h3>
+            </Link>
+          </li>
+          <li>
+            <Link href="#news">
+              <h3 onClick={toggleMenu}>Noticias</h3>
+            </Link>
+          </li>
+          <li>
+            <Link href="#contact">
+              <h3 onClick={toggleMenu}>Contacto</h3>
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   )
 }
