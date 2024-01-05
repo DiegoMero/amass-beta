562705c2ff43c6dc957fc2ca903d76cb0d838399
DiegoMero <diegomero_1999@hotmail.com>
2024-01-05 10:07:26 -0500

    Add google map

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -46,11 +46,7 @@ export default function Contact() {
       <div className='ubication'>
         <h2>Ubicación</h2>
         <p>Encuentra nuestro local más cercano:</p>
-        <div className='map-pic'
-          style={{
-            backgroundImage: `url(${map.src})`,
-          }}
-        />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d997.312131228605!2d-80.6358663!3d-0.9683352!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMMKwNTgnMDUuNCJTIDgwwrAzOCcwOC4yIlc!5e0!3m2!1ses!2sec!4v1704466717877!5m2!1ses!2sec" width="100%" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </section>
   )
