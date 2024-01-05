5ad8853c4fba5e846d36e997461ae0f46ba1b772
DiegoMero <diegomero_1999@hotmail.com>
2024-01-05 09:23:22 -0500

    Add new about section

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,14 +13,14 @@ export default function Header() {
       }}
       className='header'
     >
-      <h1>AMASSTecho</h1>
+      <h1>AMASS Cia. Ltda.</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Integer porttitor est sit amet pharetra blandit.
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        Ofrecemos soluciones de construcción innovadoras, 
+        eficientes y adaptadas a las necesidades específicas de Manabí y sus sectores productivos, 
+        contribuyendo así al desarrollo sostenible y a la construcción de la región.
       </p>
       <Link href="#products">
-        <button className={inter.className}>Conoce más</button>
+        <button className={inter.className}>Contáctanos</button>
       </Link>
     </section>
   )
