3e459bdb28f549aea7b31f3d9bb2e4e46329d8c3
DiegoMero <diegomero_1999@hotmail.com>
2024-01-05 09:59:04 -0500

    Add social links

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import './style/header.scss';
 import backgroundImage from '/public/pic3.jpg';
 import { Inter } from 'next/font/google'
-import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Header() {
+  const phoneNumber = '0992058094';
+
+  const handleContactButtonClick = () => {
+    const phoneLink = `tel:${phoneNumber}`;
+    window.location.href = phoneLink;
+  };
+
   return (
     <section
       style={{
@@ -19,9 +27,7 @@ export default function Header() {
         eficientes y adaptadas a las necesidades específicas de Manabí y sus sectores productivos, 
         contribuyendo así al desarrollo sostenible y a la construcción de la región.
       </p>
-      <Link href="#products">
-        <button className={inter.className}>Contáctanos</button>
-      </Link>
+      <button className={inter.className} onClick={handleContactButtonClick}>Contáctanos</button>
     </section>
   )
 }
