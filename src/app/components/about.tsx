ac8a556106d7fc55511f5dace2e25894a8929de6
DiegoMero <diegomero_1999@hotmail.com>
2024-03-23 08:46:12 -0500

    Add new about desktop section

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -20,15 +20,15 @@ export default function About() {
   ];
 
   return (
-    <section id="nosotros" className="flex flex-col items-center p-3">
-      <h2 className="text-mint text-3xl font-bold pt-20">Nosotros</h2>
-      <p className="text-center text-xl pt-8">
+    <section id="nosotros" className="flex flex-col items-center p-3 md:pr-20 md:pl-20 md:pb-28">
+      <h2 className="text-mint text-3xl font-bold pt-20 md:text-4xl">Nosotros</h2>
+      <p className="text-center text-xl pt-8 md:text-2xl md:pl-44 md:pr-44">
         AMASS es una empresa comprometida con el crecimiento sostenible y
         sustentable con la fabricación de productos reciclables y reusables, para
         crear productos de construcción destinados al uso de la sociedad.
       </p>
-      <h3 className="text-mint text-2xl font-bold border-b border-mint w-full text-center pt-8">Valores</h3>
-      <ul className="flex flex-col gap-5 pt-5">
+      <h3 className="text-mint text-2xl font-bold border-b border-mint w-full text-center pt-8 md:text-3xl">Valores</h3>
+      <ul className="flex flex-col gap-5 pt-5 md:flex-row md:gap-12">
         {valoresData.map((valor, index) => (
           <AboutCard key={index} title={valor.title} imagen={valor.imagen} descripcion={valor.descripcion} />
         ))}
--- src/app/components/utility/about-card.tsx
+++ src/app/components/utility/about-card.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 export default function AboutCard({ title, imagen, descripcion }: any) {
   return (
-    <li className="bg-mint text-white p-2 flex flex-col gap-3">
+    <li className="bg-mint text-white p-2 flex flex-col gap-3 md:p-10">
       <Image
         src={imagen}
         width={85}
         height={85}
         alt="Valor"
       />
-      <p>
+      <p className="md:text-xl">
         <span className="font-bold">{title} </span>
         {descripcion}
       </p>
