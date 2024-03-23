0f56c782225029e974478c0a9c81b563a1a1cdcd
DiegoMero <diegomero_1999@hotmail.com>
2024-03-23 08:04:35 -0500

    Add new desktop navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import SlideMenu from "./utility/slide-menu";
+import Link from "next/link";
 
 export default function Navbar() {
 
@@ -15,14 +16,19 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="h-[15vh] sticky top-0 bg-mint flex justify-between items-center pl-3 pr-3 shadow-xl z-10">
+      <nav className="h-[15vh] sticky top-0 bg-mint flex justify-between items-center pl-3 pr-3 shadow-xl z-10 md:pl-20 md:pr-20">
         <Image
           src="/logo-blanco.png"
-          width={40}
-          height={30}
+          width={50}
+          height={70}
           alt="AMASS Cia. Ltda."
         />
-        <FiMenu onClick={handleClick} className="text-white  active:bg-[#28568c] p-2 text-5xl rounded-full" />
+        <FiMenu onClick={handleClick} className="text-white active:bg-[#28568c] p-2 text-5xl rounded-full md:hidden" />
+        <ul className='hidden md:flex text-white text-2xl font-bold gap-10'>
+          <li className="hover:underline"><Link href="#nosotros">Nosotros</Link></li>
+          <li className="hover:underline"><Link href="#productos">Productos</Link></li>
+          <li className="hover:underline"><Link href="#contacto">Contacto</Link></li>
+        </ul>
       </nav>
       {togglemenu && <SlideMenu handleClick={handleClick} />}
     </>
--- src/app/components/utility/slide-menu.tsx
+++ src/app/components/utility/slide-menu.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export default function SlideMenu({ handleClick }: { handleClick: () => void}) {
   return (
-    <ul className="fixed top-[15vh] z-10 p-3 h-[85vh] w-screen flex  flex-col gap-8 bg-mint/90 animate-slide-in-menu">
-      <li className="text-white text-3xl ml-auto pr-2" onClick={handleClick}><RxCross1 /></li>
-      <li className="text-white text-3xl" onClick={handleClick}><Link href="#nosotros">Nosotros</Link></li>
-      <li className="text-white text-3xl" onClick={handleClick}><Link href="#productos">Productos</Link></li>
-      <li className="text-white text-3xl" onClick={handleClick}><Link href="#contacto">Contacto</Link></li> 
+    <ul className="fixed top-[15vh] z-10 p-3 h-[85vh] w-screen text-white text-3xl flex flex-col gap-8 bg-mint/90 animate-slide-in-menu">
+      <li className=" ml-auto pr-2" onClick={handleClick}><RxCross1 /></li>
+      <li onClick={handleClick}><Link href="#nosotros">Nosotros</Link></li>
+      <li onClick={handleClick}><Link href="#productos">Productos</Link></li>
+      <li onClick={handleClick}><Link href="#contacto">Contacto</Link></li> 
     </ul>
   )
 }
