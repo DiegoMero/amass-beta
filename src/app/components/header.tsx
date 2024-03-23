d683238b29b8eba94370fa114bdb673819c5e076
DiegoMero <diegomero_1999@hotmail.com>
2024-03-23 08:35:07 -0500

    Add new header desktop section

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -11,11 +11,11 @@ const amaranth = Amaranth({
 export default function Header() {
   return (
     <section className='h-[85vh]'>
-      <div className='h-[30vh] bg-gradient-to-r from-mint-left to-mint text-white p-3'>
-        <h1 className='text-3xl font-bold'>AMASS Cia. Ltda.</h1>
-        <p className={`${amaranth.className} text-2xl`}>Fabricantes directos de espumaflex y Panel EPS tipo sánduche.</p>
+      <div className='h-[30vh] md:h-[25vh] bg-gradient-to-r from-mint-left to-mint text-white p-3 md:pr-20 md:pl-20 md:flex md:flex-col md:justify-center'>
+        <h1 className='text-3xl font-bold md:text-5xl'>AMASS Cia. Ltda.</h1>
+        <p className={`${amaranth.className} text-2xl md:text-3xl`}>Fabricantes directos de espumaflex y Panel EPS tipo sánduche.</p>
       </div>
-      <div className='relative h-[55vh] flex items-end p-3'>
+      <div className='relative h-[55vh] md:h-[60vh] flex items-end p-3 md:pr-20 md:pl-20 md:pb-10'>
         <Image
           src="/Portada-WEB-AMASS-Recovered.png"
           fill
